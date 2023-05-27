349ad8817cca8925c13fb5483247df095d67c501
Alex Lapinski <alex.lapinski@gmail.com>
2023-05-26 21:03:21 -0400

    Added final integration

--- shell_chatgpt/main.py
+++ shell_chatgpt/main.py
@@ -1,4 +1,5 @@
 import openai
+import os
 import re
 import json
 
@@ -9,9 +10,31 @@ developer_assistant_prompt = """
     {
         "files": [
             {
+                "type": "directory",
+                "name": "<project_name>",
+                "files": [
+                    {
+                        "type": "file",
+                        "name": "__init__.py",
+                        "contents": ""
+                    },
+                    {
+                        "type": "file",
+                        "name": "main.py",
+                        "contents": ""
+                    }
+                ]
+            },
+            {
                 "type": "file",
-                "name": "main.py",
-                "contents": "print('hello world')"
+                "name": "README.md",
+                "contents": "# New Project"
+            },
+            {
+                "type": "file",
+                "name": "requirements.txt",
+                "contents": "[[source]]\nurl = "https://pypi.org/simple"\nverify_ssl = true\nname = "pypi"\n\n[packages]\npytest = "*"\npytest-cov = "*"\n
+\n[dev-packages]\n\n[requires]\npython_version = "3.10"\npython_full_version = "3.10.6"\n"
             },
             <Additional Files>
         ],
@@ -37,6 +60,34 @@ def extract_json(input_text):
             print('Could not parse the extracted content as JSON.')
             return None
 
+def ensure_path_exists(path):
+    try:
+        os.makedirs(path, exist_ok=True)
+    except OSError as error:
+        print(f"Creation of the directory {path} failed: {error}")
+    else:
+        print(f"Successfully created the directory {path} or it already existed")
+
+def create_files_from_json(json_obj, parent_dir=''):
+    for file_obj in json_obj['files']:
+        if file_obj['type'] == 'directory':
+            new_dir_path = os.path.join(parent_dir, file_obj['name'])
+            ensure_path_exists(new_dir_path)
+            create_files_from_json(file_obj, parent_dir=new_dir_path)
+        elif file_obj['type'] == 'file':
+            file_path = os.path.join(parent_dir, file_obj['name'])
+            with open(file_path, 'w') as f:
+                f.write(file_obj['contents'])
+
+def ensure_path_exists(path: str):
+    try:
+        os.makedirs(path, exist_ok=True)
+    except OSError as error:
+        print(f"Creation of the directory {path} failed: {error}")
+    else:
+        print(f"Successfully created the directory {path} or it already existed")
+
+
 def chat_with_gpt3():
     while True:
         prompt = input('> ')
@@ -58,7 +109,11 @@ def chat_with_gpt3():
         content = response['choices'][0]['message']['content']
         print(content)
 
-        print(extract_json(content))
+        json_content = extract_json(content)
+
+        print("Creating Folders and files")
+        create_files_from_json(json_content)
+
 
         #files = content["files"]
         #directions = content["directions"]
